3862e8aca364db1a2056b14d827d02294c70e2dd
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2025-05-25 18:04:12 +0530

    Problem 1

--- problems/arrays/contains_duplicate.cpp
+++ problems/arrays/contains_duplicate.cpp
@@ -1,10 +1,58 @@
-#include<iostream>
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
-class Solution{
+class solution {
 public:
-    bool containsDuplicate(vector<int> &nums) {
+    bool containsduplicate(vector<int> &nums) {
+        unordered_set<int> hashset;
 
-    }
+        for (int n : nums) {
+            if (hashset.find(n) != hashset.end()) {
+                return true;
+            }
+            hashset.insert(n);
+
+            return false;
+        }
+    };
 };
+
+TEST(ContainsDuplicateTest, NoDuplicates) {
+    solution sol;
+    vector<int> nums = {1, 2, 3, 4};
+    EXPECT_FALSE(sol.containsduplicate(nums));
+}
+
+TEST(ContainsDuplicateTest, HasDuplicates) {
+    solution sol;
+    vector<int> nums = {1, 2, 3, 2};
+    EXPECT_TRUE(sol.containsduplicate(nums));
+}
+
+TEST(ContainsDuplicateTest, EmptyVector) {
+    solution sol;
+    vector<int> nums = {};
+    EXPECT_FALSE(sol.containsduplicate(nums));
+}
+
+TEST(ContainsDuplicateTest, SingleElement) {
+    solution sol;
+    vector<int> nums = {42};
+    EXPECT_FALSE(sol.containsduplicate(nums));
+}
+
+TEST(ContainsDuplicateTest, AllSame) {
+    solution sol;
+    vector<int> nums = {7, 7, 7, 7};
+    EXPECT_TRUE(sol.containsduplicate(nums));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
